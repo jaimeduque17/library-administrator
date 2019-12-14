576378fe7a348321b57135272ef7f7e805d75975
Jaime Duque <jaime@Jaimes-MacBook-Pro.local>
2019-12-13 22:31:39 -0500

    Navbar is created

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import EditSubscriber from './components/subscribers/EditSubscriber';
 import NewSubscriber from './components/subscribers/NewSubscriber';
 import ShowSubscriber from './components/subscribers/ShowSubscriber';
 import Subscribers from './components/subscribers/Subscribers';
 
+import Navbar from './components/layout/Navbar';
+
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/subscribers" component={Subscribers} />
-        <Route exact path="/subscribers/new" component={NewSubscriber} />
-        <Route exact path="/subscribers/show/:id" component={ShowSubscriber} />
-        <Route exact path="/subscribers/edit/:id" component={EditSubscriber} />
-      </Switch>
+      <Navbar />
+      <div className="container">
+        <Switch>
+          <Route exact path="/subscribers" component={Subscribers} />
+          <Route exact path="/subscribers/new" component={NewSubscriber} />
+          <Route exact path="/subscribers/show/:id" component={ShowSubscriber} />
+          <Route exact path="/subscribers/edit/:id" component={EditSubscriber} />
+        </Switch>
+      </div>
     </Router>
   );
 }
