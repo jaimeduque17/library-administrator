2e16db9145702d2efdd487221c37e22ec342601c
Jaime Duque <jaime@Jaimes-MacBook-Pro.local>
2019-12-17 23:29:31 -0500

    The connection between Firestore with React and Redux is created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import store from './store';
+import { Provider } from 'react-redux';
 
 import EditSubscriber from './components/subscribers/EditSubscriber';
 import NewSubscriber from './components/subscribers/NewSubscriber';
@@ -10,17 +12,19 @@ import Navbar from './components/layout/Navbar';
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <div className="container">
-        <Switch>
-          <Route exact path="/subscribers" component={Subscribers} />
-          <Route exact path="/subscribers/new" component={NewSubscriber} />
-          <Route exact path="/subscribers/show/:id" component={ShowSubscriber} />
-          <Route exact path="/subscribers/edit/:id" component={EditSubscriber} />
-        </Switch>
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/subscribers" component={Subscribers} />
+            <Route exact path="/subscribers/new" component={NewSubscriber} />
+            <Route exact path="/subscribers/show/:id" component={ShowSubscriber} />
+            <Route exact path="/subscribers/edit/:id" component={EditSubscriber} />
+          </Switch>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
---
+++ src/store.js
@@ -0,0 +1,49 @@
+import { createStore, combineReducers, compose } from 'redux';
+import { reactReduxFirebase, firebaseReducer } from 'react-redux-firebase';
+import { reduxFirestore, firestoreReducer } from 'redux-firestore';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+// firestore configuration
+const firebaseConf = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "l",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
+}
+
+// firebase initialization
+firebase.initializeApp(firebaseConf);
+
+// react-redux config
+const rrfConfig = {
+    userProfile: 'users',
+    useFirestoreForProfile: true
+}
+
+// enhancer with compose of redux and firebase
+const createStoreWithFirebase = compose(
+    reactReduxFirebase(firebase, rrfConfig),
+    reduxFirestore(firebase)
+)(createStore);
+
+// reducers
+const rootReducer = combineReducers({
+    firebase: firebaseReducer,
+    firestore: firestoreReducer
+})
+
+// initial state
+const initialState = {};
+
+// create store
+const store = createStoreWithFirebase(rootReducer, initialState, compose(
+    reactReduxFirebase(firebase),
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+));
+
+export default store;
