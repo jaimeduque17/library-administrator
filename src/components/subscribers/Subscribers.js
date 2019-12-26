5caa773cb9ba42d4857892168398ad07b823b332
Jaime Duque <jaime.duque@estratek.com.co>
2019-12-26 18:44:00 -0500

    Subscribers are showed

--- src/components/subscribers/Subscribers.js
+++ src/components/subscribers/Subscribers.js
@@ -1,9 +1,61 @@
 import React from 'react';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import { Link } from 'react-router-dom';
 
-const Subscribers = () => {
+const Subscribers = ({subscribers}) => {
+
+console.log('subscribers', subscribers);
+    if(!subscribers) return <h1>Loading...</h1>;
+
     return (
-        <h1>Subscribers</h1>
+        <div className="row">
+            <div className="col-md-12 mb-4">
+                <Link
+                to="/subscribers/new"
+                className="btn btn-primary"
+                >
+                    <i className="fas fa-plus"></i> New Subscriber
+                </Link>
+            </div>
+            <div className="col-md-8">
+                <h2>
+                    <i className="fas fa-users"></i> Subscribers
+                </h2>
+            </div>
+            <table className="table table-striped mt-4">
+                <thead className="text-light bg-primary">
+                    <tr>
+                        <th>Name</th>
+                        <th>Career</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {subscribers.map(subscriber => (
+                        <tr key={subscriber.id}>
+                            <td>{`${subscriber.name} ${subscriber.lastName}`}</td>
+                            <td>{subscriber.career}</td>
+                            <td>
+                                <Link
+                                to={`/subscribers/show/${subscriber.id}`}
+                                className="btn btn-success btn-block"
+                                >
+                                    <i className="fas fa-angle-double-right"></i> More Information
+                                </Link>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
     );
 }
 
-export default Subscribers;
+export default compose(
+    firestoreConnect([{collection: 'subscribers'}]),
+    connect((state, props) => ({
+        subscribers: state.firestore.ordered.subscribers
+    }))
+)(Subscribers);
